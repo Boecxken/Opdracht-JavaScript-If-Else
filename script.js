b7c3a440695e16c9e52f994f1f14abe51d29db55
Boecxken <84395269+Boecxken@users.noreply.github.com>
2021-06-05 12:16:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age > 17) {
+
+    console.log("Kom maar binnen!");
+
+}
+
+else {
+
+    console.log("Je mag niet naar binnen!");
+
+}
+
+if (isFemale) {
+
+    console.log("Welkom!");
+}
+
+else {
+
+    console.log("Helaas!");
+
+}
+
+if (driverStatus == "bob") {
+
+    console.log("Je mag rijden!")
+}
+
+else {
+
+    console.log("Neem een taxi.")
+
+}
